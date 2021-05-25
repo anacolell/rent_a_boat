b7183d5749b327a513927b6fb1e69a8379295974
ana colell <anacolell8@gmail.com>
2021-05-25 15:37:42 +0200

    Fixed validations on booking model

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -4,8 +4,8 @@ class Booking < ApplicationRecord
 
   validates :user, presence: true
   validates :boat, presence: true
-  validates :start_date, :end_date, presence: true, availability: true
-  validates :end_date_after_start_date
+  validates :start_date, :end_date, presence: true
+  # validates :end_date_after_start_date
   validates :total_price, presence: true, numericality: { only_integer: true }
   validates :status, presence: true
 end
