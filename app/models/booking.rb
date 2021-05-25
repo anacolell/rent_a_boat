07b5ad41145e69ee7b908a2419843df9d9863319
ana colell <anacolell8@gmail.com>
2021-05-25 15:27:13 +0200

    Fixed typo on booking model

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,9 +3,9 @@ class Booking < ApplicationRecord
   belongs_to :boat
 
   validates :user, presence: true
-  validate :boat, presence: true
+  validates :boat, presence: true
   validates :start_date, :end_date, presence: true, availability: true
-  validate :end_date_after_start_date
+  validates :end_date_after_start_date
   validates :total_price, presence: true, numericality: { only_integer: true }
   validates :status, presence: true
 end
