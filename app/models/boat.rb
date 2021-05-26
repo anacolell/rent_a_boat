99c2f72d4924fe5a9f5b98c24c2c0646fe135ad2
ana colell <anacolell8@gmail.com>
2021-05-26 17:14:09 +0200

    Commented boat type

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -6,7 +6,7 @@ class Boat < ApplicationRecord
   validates :availability, presence: true
   validates :capacity, presence: true, numericality: { only_integer: true }
   validates :size, presence: true, numericality: { only_integer: true }
-  validates :boat_type, presence: true, inclusion: { in: TYPE }
+  # validates :boat_type, presence: true, inclusion: { in: TYPE }
   validates :description, presence: true
 
   belongs_to :user
