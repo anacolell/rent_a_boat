7d25fdf1dbf1c5de1f9663a31cf6b314def35934
Britta Felzmann <brittafelzmann97@gmail.com>
2021-05-26 13:57:54 +0200

    Implemented show page

--- app/controllers/boats_controller.rb
+++ app/controllers/boats_controller.rb
@@ -1,8 +1,13 @@
 class BoatsController < ApplicationController
-  skip_before_action :authenticate_user!, only: %i[home index]
+  skip_before_action :authenticate_user!, only: %i[home index show] # add more methods if needed
+  before_action :set_boat, only: %i[show] # add more methods if needed
+
   def index
     @boats = policy_scope(Boat)
     # authorize @boats
+  end
+
+  def show
   end
 
   def new
@@ -20,6 +25,11 @@ class BoatsController < ApplicationController
   end
 
   private
+
+  def set_boat
+    @boat = Boat.find(params[:id])
+    authorize @boat
+  end
 
   def boat_params
     params.require(:boat).permit(:price, :name, :location, :availability, :capacity, :size, :boat_type, :description)
